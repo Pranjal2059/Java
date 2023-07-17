dea541297e04af2fd7979b608b3dddd6a706a80e
Pranjal2059 <114998365+Pranjal2059@users.noreply.github.com>
2023-07-17 23:31:42 +0545

    Create string2.java

---
+++ String/string2.java
@@ -0,0 +1,20 @@
+public class string2{
+    public  static void main(string[]args){
+        
+        String w1="useState";
+        String w3= w1.toLowerCase();
+        System.out.println(w3);
+        
+        String w="ohio";
+        String w4=w.toUpperCase();
+        System.out.println(w4);
+        
+        String word=" Hi I am Pranjal ";
+        String word1=word.trim();
+        System.out.println(word1);
+        
+        String s1="hello warld";
+        String s2= s1.replace('a','o');
+        System.out.println(s2);
+    }
+}
