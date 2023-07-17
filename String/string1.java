7dde5c1bb542ec0b674a327ec2965e59cd6938fd
Pranjal2059 <114998365+Pranjal2059@users.noreply.github.com>
2023-07-17 23:26:26 +0545

    Create string1.java

---
+++ String/string1.java
@@ -0,0 +1,15 @@
+public class string{
+    public static void main(String[]args){
+        String s= "HelloPranjal";
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.charAt(5));
+        System.out.println(s.substring(6));
+        System.out.println(s.substring(2,6));
+        
+        String s1="Hello";
+        String s2="world";
+        String output=s1.concat(s2);
+        System.out.println(output);   
+    }  
+}
